fb8b6168b6e66913156aebf9f7c0f05bdd7b0712
Bill Matera <matera@optonline.net>
2018-09-28 22:26:39 -0400

    9/28/18 updates

---
+++ control-flow/6-countTruthy-bm.js
@@ -0,0 +1,14 @@
+//falsey values = undefined, null, '', false, 0, NaN.  Everything else is a truthy.
+const myArray = [1, 2, 3, 5, true, null, undefined, NaN, '', false, 0];
+console.log(countTruthy(myArray));
+
+function countTruthy (array) {
+  let count=0;  
+  for (let value of array) {
+    if (value)      
+      count++;
+      //JS engine will interpret as truthy or falsey. No need to check for all falsey/truthy conditions.
+      //Remember, it doesn't have to be boolean as JS engine will determine if truthy or falsey.
+  }
+  return count;
+}
